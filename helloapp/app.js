0c76139ae4507b3e51f3edc35cd5171be9b5e374
PavelSolovev <pashius2010@gmail.com>
2020-07-27 01:24:16 +0400

    modules

--- helloapp/app.js
+++ helloapp/app.js
@@ -1,10 +1,4 @@
-const os = require("os");
-const greeting = require("./greeting");
-const User = require("./user.js");
+const welcome = require("./welcome");
 
-let userName = os.userInfo().username;
-console.log(`Дата запроса: ${greeting.date}`);
-console.log(greeting.getMessage(userName));
-
-let pavel = new User("Pavel", 35);
-pavel.sayHi();
+welcome.getMorningMessage();
+welcome.getEveningMessage();
