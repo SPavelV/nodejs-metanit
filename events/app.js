713f44ba127364bcc3289c9f4d6a08190c0d8e99
Pavel Solovev <pashius2010@gmail.com>
2021-04-22 09:32:54 +0400

    inheritance from event

--- events/app.js
+++ events/app.js
@@ -6,3 +6,14 @@ emitter.on(eventName, (data) => console.log(data));
 emitter.on(eventName, () => console.log("Привет!"));
 
 emitter.emit(eventName, "Приве пир");
+
+class User extends Emitter {
+  sayHi(data) {
+    this.emit(eventName, data);
+  }
+}
+
+let user = new User();
+user.on(eventName, (data) => console.log(data));
+
+user.sayHi("Мне нужна твоя одежда...");
