77a4080befe752ddd1e72647617180bcf3f58108
Pavel Solovev <pashius2010@gmail.com>
2021-04-23 01:35:55 +0400

    middleware for logging requests

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
+app.use(function (request, response, next) {
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let data = `${hour}:${minutes}:${seconds} ${request.method} ${
+    request.url
+  } ${request.get("user-agent")}`;
+  console.log(data);
+  fs.appendFile("server.log", data + "\n", function () {});
+});
 
 app.get("/", function (request, response) {
-  response.send("<h2>Привет Express!</h2>");
-});
-app.get("/about", function (request, response) {
-  response.send("<h2>О сайте</h2>");
-});
-app.get("/contact", function (request, response) {
-  response.send("<h2>Контакты</h2>");
+  response.send("<h2>Hello</h2>");
 });
 
 app.listen(3000);
