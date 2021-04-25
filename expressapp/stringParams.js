dddbeb0a1bf89d2473da4c7005ca3a0e10ada40b
Pavel Solovev <pashius2010@gmail.com>
2021-04-26 00:28:28 +0400

    pass arrays

--- expressapp/stringParams.js
+++ expressapp/stringParams.js
@@ -14,4 +14,16 @@ app.use("/about", function (request, response) {
   );
 });
 
+app.use("/names", function (request, response) {
+  console.log(request.query);
+  let names = request.query.name;
+  let responseText = "<ul>";
+  for (let i = 0; i < names.length; i++) {
+    responseText += "<li>" + names[i] + "</li>";
+  }
+
+  responseText += "</ul>";
+  response.send(responseText);
+});
+
 app.listen(3000);
