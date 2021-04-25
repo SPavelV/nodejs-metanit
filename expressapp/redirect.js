85c600028b7edefe9895fc6be09622c14a375074
Pavel Solovev <pashius2010@gmail.com>
2021-04-25 23:15:07 +0400

    redirect

---
+++ expressapp/redirect.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use("/index", function (request, response) {
+  response.redirect("https://metanit.com");
+});
+
+app.listen(3000);
