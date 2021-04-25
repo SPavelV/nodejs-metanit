17c6c58be831343ea772ab02907bb238651d39e4
Pavel Solovev <pashius2010@gmail.com>
2021-04-26 00:18:07 +0400

    params string qurey

---
+++ expressapp/stringParams.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (request, response) {
+  response.send("<h1>Главная страница</h1>");
+});
+
+app.use("/about", function (request, response) {
+  const id = request.query.id;
+  const userName = request.query.name;
+
+  response.send(
+    "<h1>Информация</h1><p>id=" + id + "</p><p>name=" + userName + "</p>"
+  );
+});
+
+app.listen(3000);
