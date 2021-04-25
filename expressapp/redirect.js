08695ce76a901c87c56c0af65c2b76e894ff4518
Pavel Solovev <pashius2010@gmail.com>
2021-04-25 23:40:32 +0400

    redirect cases

--- expressapp/redirect.js
+++ expressapp/redirect.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
 
-app.use("/index", function (request, response) {
-  response.redirect("https://metanit.com");
+// redirect on http://localhost:3000/home
+app.use("home/foo/bar", function (requrest, response) {
+  response.redirect("..");
+});
+
+// redirect on http://localhost:3000/home/foo
+app.use("/home/foo/bar", function (request, response) {
+  response.redirect(".");
+});
+
+// redirect on http://localhost:3000/home/about
+app.use("/home/foo/bar", function (request, response) {
+  response.redirect("../about");
+});
+
+// redirect on http://localhost:3000/home/foo/about
+app.use("/home/foo/bar", function (request, response) {
+  response.redirect("./about");
+});
+
+// redirect on http://localhost:3000/about
+app.use("/home/bar", function (request, response) {
+  response.redirect("/about");
+});
+
+// redirect on http://localhost:3000/home/about
+app.use("/home/bar", function (request, response) {
+  response.redirect("about");
+});
+
+app.use("/about", function (request, response) {
+  response.send("<h1>About</h1>");
 });
 
 app.listen(3000);
