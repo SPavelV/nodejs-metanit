0d591a47f75f16f8612049f515e661cc78b4c07f
Pavel Solovev <p.soloviev@galament.com>
2021-05-03 16:01:18 +0400

    add routes

--- mvcPattern/app.js
+++ mvcPattern/app.js
@@ -1,23 +1,11 @@
 const express = require("express");
 const app = express();
-const userController = require("./controllers/userController");
-const homeController = require("./controllers/homeController");
+const userRouter = require("./routes/userRouter");
+const homeRouter = require("./routes/homeRouter");
 
-// определяем роутеры
-const userRouter = express.Router();
-const homeRouter = express.Router();
-
-// определяем маршруты и их обработчики внутри роутера userRouter
-userRouter.use("/create", userController.addUser);
-userRouter.use("/", userController.getUsers);
 app.use("/users", userRouter);
-
-// определяем маршруты и их обработчики внутри роутера homeRouter
-app.get("/about", homeController.about);
-app.get("/", homeController.index);
 app.use("/", homeRouter);
 
-// обработка ошибок
 app.use(function (req, res, next) {
   res.status(404).send("Not found");
 });
