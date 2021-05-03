b9d0e172daca3c69ae8d81b0b8786f2e4201a2e0
Pavel Solovev <p.soloviev@galament.com>
2021-05-03 15:41:43 +0400

    add controllers

--- mvcPattern/app.js
+++ mvcPattern/app.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const app = express();
+const userController = require("./controllers/userController");
+const homeController = require("./controllers/homeController");
 
 // определяем роутеры
-const userRouter = express.Router(); // для адресов "/users"
+const userRouter = express.Router();
+const homeRouter = express.Router();
 
-userRouter.use("/create", function (req, res) {
-  res.send("Добавления пользователя");
-});
-userRouter.use("/", function (req, res) {
-  res.send("Список пользователей");
-});
-// сопоставляем роут с конечной точкой "/users"
+// определяем маршруты и их обработчики внутри роутера userRouter
+userRouter.use("/create", userController.addUser);
+userRouter.use("/", userController.getUsers);
 app.use("/users", userRouter);
 
-// общие обработчики
-app.get("/about", function (req, res) {
-  res.send("О сайте");
-});
-
-app.get("/", function (req, res) {
-  res.send("Главная страница");
-});
+// определяем маршруты и их обработчики внутри роутера homeRouter
+app.get("/about", homeController.about);
+app.get("/", homeController.index);
+app.use("/", homeRouter);
 
 // обработка ошибок
 app.use(function (req, res, next) {
