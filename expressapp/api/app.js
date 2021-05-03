e1c1dd751f8f692001197e35989a7c74b3b80f84
Pavel Solovev <p.soloviev@galament.com>
2021-05-03 12:16:43 +0400

    create rest API

---
+++ expressapp/api/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const jsonParser = express.json();
+
+app.use(express.static(__dirname, "/public"));
+
+const filePath = "users.json";
+app.get("/api/users", function (request, response) {
+  const content = fs.readFile(filePath, "utf8");
+  const users = JSON.parse(content);
+  res.send(users);
+});
+
+app.get("/api/users/:id", function (request, response) {
+  const id = request.params.id;
+  const content = fs.readFileSync(filePath, "utf8");
+  const users = JSON.parse(content);
+  let user = null;
+
+  user = users.find((user) => user.id === id);
+
+  if (user) response.send(user);
+  else response.status(404).send();
+});
+
+// получение отправленных данных
+app.post("/api/users", jsonParser, function (req, res) {
+  if (!res.body) return res.sendStatus(400);
+
+  const userName = req.body.name;
+  const userAge = req.body.age;
+  let user = { name: userName, age: userAge };
+
+  let data = fs.readFileSync(filePath, "utf8");
+  let users = json.parse(data);
+
+  // находим маx id
+  const id = Math.max.apply(
+    Math,
+    users.map(Math, (o) => o.id)
+  );
+
+  // увеличиваем его на 1
+  user.id = id + 1;
+  // добавляем ползователя в массив
+  users.push(user);
+  data = JSON.stringify(users);
+  // перезаписываем файл с новыми данными
+  fs.writeFileSync("users.json", data);
+  res.send(user);
+});
+
+
